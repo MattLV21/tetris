8ee24ced5a4f5a450c4cf05c7a4e73619945ac12
MattLV21 <59840529+MattLV21@users.noreply.github.com>
2022-11-17 00:09:25 +0100

    Add files via upload

---
+++ board.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from pieces import Piece, rotate, make_piece, pieces
+EMPTY = 0
+TAKEN = 1
+HOVERING = 2
+
+@dataclass
+class Board:
+    """ tetris board """
+    board: list[list[int]]
+    piece: Piece
+
+def make_board() -> Board:
+    """ creats a empty tetris board """
+    b = []
+    for i in range(20):
+        row = []
+        for j in range(10):
+            row.append(EMPTY)
+        b.append(row)
+    return Board(b, Piece(None, -1, -1))
+
+def update_piece(b: Board) -> None:
+    """ draws the moveing piece """
+    for idx1, row in enumerate(board.board):
+        for idx2, col in enumerate(row):
+            if board.board[idx1][idx2] == HOVERING:
+                board.board[idx1][idx2] = EMPTY
+
+    for idx1, row in enumerate(board.board):
+        for idx2, col in enumerate(row):
+            if idx1 == board.piece.y and idx2 == board.piece.x:
+                for ii, i in enumerate(board.piece.shape):
+                    board.board[idx1 + i[1]][idx2 + i[0]] = HOVERING
+                break
+
+def line_break(b: Board) -> int:
+    """ returns the row that a line has acored 
+    returns -1 if no lines have acored """
+    l = -1
+    for idx, row in enumerate(b.board):
+        if 0 not in row:
+            l = idx
+    return l
+
+def remove_line(b: Board, line: int) -> None:
+    """ removes a line from board and
+    pushes all lines above down """
+    for i in range(line, 20):
+        b.board[i] = b.board[i+1]
+
+def move_down(b: Board) -> None:
+    """ moves the piece down one line """
+    b.piece.y = b.piece.y + 1
+def move_side(b: Board, d: int) -> None:
+    """ moves the piece to the left or right """
+    b.piece.x = b.piece.x + d
+
+
+board = make_board()
+board.piece = make_piece(pieces.__getitem__('cube'))
+update_piece(board)
+for i in range(3):
+    for row in board.board: 
+        print(row)
+    move_down(board)
+    update_piece(board)
+    print()
+rotate(board.piece)
+rotate(board.piece)
+update_piece(board)
+for row in board.board:
+        print(row)
---
+++ pieces.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class Piece:
+    """ a tetris block/piece """
+    shape: list[list[int]]
+    x: int
+    y: int
+
+def rotate(p: Piece) -> None:
+    """ rotates a piece (transpose) """
+    for i in p.shape:
+        i[0], i[1] = i[1], i[0]
+
+def make_piece(s: list[list[int]]) -> Piece:
+    """ spawns a new piece """
+    # 5 to start in the middle 
+    return Piece(s, 0, 0) 
+
+# tetris pieces start coords
+pieces = {
+    'strigt_line':[[0, 0], [0, 1], [0, 2], [0, 3]],
+    'cube':[[0, 0], [0, 1], [1, 0], [1, 1]],
+    'left_l':[[0, 0], [0, 1], [0, 2], [1, 2]],
+    'right_l':[[1, 0], [1, 1], [1, 2], [0, 2]]
+}
